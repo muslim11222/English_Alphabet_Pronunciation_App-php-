0980972c889aab006af66d4013970b086613fb39
muslilm <muslim9900khan@gmail.com>
2024-12-02 09:09:02 -1200

    first commit

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>English Alphabet Pronunciation App</title>
+     <link rel="stylesheet" href="/English_Alphabet_Pronunciation_App/style.css">
+</head>
+<body>
+     <h1> English Alphabet Pronunciation App </h1>
+
+
+
+
+     <div class="main_container">
+          <h2> Press the button to pronounce </h2>
+
+
+          <!-- form area -->
+          <form action="index.php" method="POST">
+
+               <input type="submit" name="a" value="A">
+               <input type="submit" name="b" value="B">
+               <input type="submit" name="c" value="C">
+               <input type="submit" name="d" value="D">
+               <input type="submit" name="e" value="E">
+               <input type="submit" name="f" value="F">
+               <input type="submit" name="g" value="G">
+               <input type="submit" name="h" value="H">
+               <input type="submit" name="i" value="I">
+               <input type="submit" name="j" value="J">
+               <input type="submit" name="k" value="K">
+               <input type="submit" name="l" value="L">
+               <input type="submit" name="m" value="M">
+               <input type="submit" name="n" value="N">
+               <input type="submit" name="o" value="O">
+               <input type="submit" name="p" value="P">
+               <input type="submit" name="q" value="Q">
+               <input type="submit" name="r" value="R">
+               <input type="submit" name="s" value="S">
+               <input type="submit" name="t" value="T">
+               <input type="submit" name="u" value="U">
+               <input type="submit" name="v" value="V">
+               <input type="submit" name="w" value="W">
+               <input type="submit" name="x" value="X">
+               <input type="submit" name="y" value="Y">
+               <input type="submit" name="z" value="Z">
+
+          </form>
+     </div>
+
+
+     <?php 
+
+          if(isset($_POST['a'])) {
+               ?>
+                    <video src="video/a.mp4" type="video/mp4" autoplay hidden>
+
+                    </video>
+               <?php
+          }
+
+          // section b
+          if(isset($_POST['b'])) {
+               ?>
+                    <video src="video/b.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+          // section c
+          if(isset($_POST['c'])) {
+               ?>
+                    <video src="video/c.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section d
+          if(isset($_POST['d'])) {
+               ?>
+                    <video src="video/d.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+           // section e
+           if(isset($_POST['e'])) {
+               ?>
+                    <video src="video/e.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section f
+          if(isset($_POST['f'])) {
+               ?>
+                    <video src="video/f.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section g
+          if(isset($_POST['g'])) {
+               ?>
+                    <video src="video/g.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          // section h
+          if(isset($_POST['h'])) {
+               ?>
+                    <video src="video/h.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section i
+          if(isset($_POST['i'])) {
+               ?>
+                    <video src="video/i.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section j
+          if(isset($_POST['j'])) {
+               ?>
+                    <video src="video/j.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section k
+          if(isset($_POST['k'])) {
+               ?>
+                    <video src="video/k.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          
+          // section l
+          if(isset($_POST['l'])) {
+               ?>
+                    <video src="video/l.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          // section m
+          if(isset($_POST['m'])) {
+               ?>
+                    <video src="video/m.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          
+          // section n
+          if(isset($_POST['n'])) {
+               ?>
+                    <video src="video/n.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+           
+          // section o
+          if(isset($_POST['o'])) {
+               ?>
+                    <video src="video/o.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+            // section p
+            if(isset($_POST['p'])) {
+               ?>
+                    <video src="video/p.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          
+          // section q
+            if(isset($_POST['q'])) {
+               ?>
+                    <video src="video/q.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          
+          // section r
+          if(isset($_POST['r'])) {
+               ?>
+                    <video src="video/r.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+
+          // section s
+          if(isset($_POST['s'])) {
+               ?>
+                    <video src="video/s.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          
+          // section t
+          if(isset($_POST['t'])) {
+               ?>
+                    <video src="video/t.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          // section u
+          if(isset($_POST['u'])) {
+               ?>
+                    <video src="video/u.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          
+          // section v
+          if(isset($_POST['v'])) {
+               ?>
+                    <video src="video/v.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          
+          // section w
+          if(isset($_POST['w'])) {
+               ?>
+                    <video src="video/w.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          // section x
+          if(isset($_POST['x'])) {
+               ?>
+                    <video src="video/x.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+          
+          // section y
+          if(isset($_POST['y'])) {
+               ?>
+                    <video src="video/y.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+          // section z
+          if(isset($_POST['z'])) {
+               ?>
+                    <video src="video/z.mp4" type="video/mp4" autoplay hidden>
+                         
+                    </video>
+               <?php
+          }
+
+
+
+
+
+
+
+     ?>
+</body>
+</html>
